7d98b8b7f8cb5acbcd66a1bf5c3b1b92ac362c8a
Prabhu Subramanian <prabhu@colearnr.com>
2016-05-29 01:17:27 +0100

    Update mongojs

--- src/db.js
+++ src/db.js
@@ -1,18 +1,19 @@
 'use strict'
-let nconf = require('nconf')
-let numCPUs = require('os').cpus().length
-let maxPoolSize = nconf.get('db_pool_size') || 10
-let authPrefix = ''
-let username = nconf.get('MONGO_USERNAME') || nconf.get('mongo_username')
-let password = nconf.get('MONGO_PASSWORD') || nconf.get('mongo_password')
+const nconf = require('nconf')
+const numCPUs = require('os').cpus().length
+const maxPoolSize = nconf.get('db_pool_size') || 10
+const authPrefix = ''
+const username = nconf.get('MONGO_USERNAME') || nconf.get('mongo_username')
+const password = nconf.get('MONGO_PASSWORD') || nconf.get('mongo_password')
 if (nconf.get('use_cluster')) {
   maxPoolSize = Math.round(maxPoolSize / numCPUs)
 }
 if (username && password) {
   authPrefix = username + ':' + password + '@'
 }
-let databaseURI = authPrefix + (process.env.MONGO_HOST || nconf.get('mongo:host') || 'localhost') + '/' + (process.env.MONGO_DB_NAME || nconf.get('mongo:database') || 'colearnr') + '?slaveOk=true&maxPoolSize=' + maxPoolSize
-let collections = ['users', 'topics', 'learnbits', 'urls']
-let db = require('mongojs').connect(databaseURI, collections)
+const mongojs = require('mongojs')
+const databaseURI = authPrefix + (process.env.MONGO_HOST || nconf.get('mongo:host') || 'localhost') + '/' + (process.env.MONGO_DB_NAME || nconf.get('mongo:database') || 'colearnr') + '?slaveOk=true&maxPoolSize=' + maxPoolSize
+const collections = ['users', 'topics', 'learnbits', 'urls']
+const db = mongojs(databaseURI, collections)
 
 module.exports = db
